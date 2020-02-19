15e626225852b4d8236166dec469476145cb62f8
James Oddy <jamesoddy8@gmail.com>
2020-02-19 15:56:00 +0000

    step 10 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require "docking_station"
 describe DockingStation do
   it { is_expected.to(respond_to(:release_bike)) }
+  describe DockingStation.new.release_bike do
+    it "gives a bike that" do is_expected.to(respond_to(:working?))
+    end
+  end
 end
