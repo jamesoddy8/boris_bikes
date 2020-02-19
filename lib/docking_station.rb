76df0d21ec8336d9df6d3efc5c56504b30641e25
James Oddy <jamesoddy8@gmail.com>
2020-02-19 17:47:46 +0000

    step 12 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
-  @bikes = []
+  def initialize
+    @bikes = []
+  end
   attr_reader :bikes
   def dock(bike)
     @bikes << bike
   end
 
   def release_bike
+    raise "No bikes left" unless @bikes.any?
     Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,7 @@ describe DockingStation do
   it { is_expected.to(respond_to(:dock)) }
   it { expect(subject).to(have_attributes(bikes: nil)) }
   it { is_expected.to(respond_to(:dock).with(1).argument) }
+  it { expect {subject.release_bike}.to raise_exception(StandardError) }
   describe DockingStation.new.release_bike do
     it "gives a bike that" do
       is_expected.to(respond_to(:working?))
